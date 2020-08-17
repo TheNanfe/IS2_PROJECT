524dcb7e52fa64c8ddd3f74e01513dae30fe3149
gimeneca <gimeneca@gmail.com>
2020-08-17 01:03:04 -0400

    commit and models proyecto, usuario, rol

--- apps/proyecto/models.py
+++ apps/proyecto/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Proyecto (models.Model):
+    Proyecto_id = models.Charfield(nax_length = 10 ,primary_key = True) 
+    nombre = models.CharField(max_length = 50)
+    descripcion = models.CharField(max_length = 50)

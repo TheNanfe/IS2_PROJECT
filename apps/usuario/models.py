92827f15056ebee72d79037b24c09e0cf900cf63
gimeneca <gimeneca@gmail.com>
2020-08-17 01:30:20 -0400

    comiit error makemigrations

--- apps/usuario/models.py
+++ apps/usuario/models.py
@@ -1,6 +1,6 @@
 from django.db import models 
 
 class Usuario(models.Model):
-    Usuario_id = models.CharField(max_length = 10 , primary_key = True)
+    Usuario_id = models.CharField(max_length = 10, primary_key=True)
     nombre = models.CharField(max_length = 50) 
     descripcion = models.CharField(max_length = 50)    
